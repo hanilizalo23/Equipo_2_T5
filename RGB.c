ef499bd95756130622a846a71d577c6934f7e8ff
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-02-15 22:18:17 -0600

    corrected comments

--- RGB.c
+++ RGB.c
@@ -1,9 +1,10 @@
-/*
- * RGB.c
- *
- *  Created on: 13 feb 2022
- *      Author: Mauricio Peralta O.
- */
+/**
+       \file
+       \brief
+                    This is the source file for the colors needed in the main implementation.
+       \author Mauricio Peralta Osorio
+       \date 15/02/2022
+*/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -15,74 +16,57 @@
 
 #define DELAY_CONST 600000 //To wait
 
-void green(void)
+void green(void) /**Turn on the green LED, waiting and turning it off*/
 {
-	GPIO_clear_pin(GPIO_E, bit_26);
-	delay(DELAY_CONST);
-	GPIO_set_pin(GPIO_E, bit_26);
+       GPIO_clear_pin(GPIO_E, bit_26);
+       delay(DELAY_CONST);
+       GPIO_set_pin(GPIO_E, bit_26);
 }
 
 
-void blue(void)
+void blue(void) /**Turn on the blue LED, waiting and turning it off*/
 {
-	GPIO_clear_pin(GPIO_B, bit_21);
-	delay(DELAY_CONST);
-	GPIO_set_pin(GPIO_B, bit_21);
+       GPIO_clear_pin(GPIO_B, bit_21);
+       delay(DELAY_CONST);
+       GPIO_set_pin(GPIO_B, bit_21);
 }
 
 
-void purple(void)
+void purple(void) /**Turn on the purple LED, waiting and turning it off*/
 {
-	GPIO_clear_pin(GPIO_B, bit_21);
-	GPIO_clear_pin(GPIO_B, bit_22);
-	delay(DELAY_CONST);
-	GPIO_set_pin(GPIO_B, bit_21);
-	GPIO_set_pin(GPIO_B, bit_22);
+       GPIO_clear_pin(GPIO_B, bit_21);
+       GPIO_clear_pin(GPIO_B, bit_22);
+       delay(DELAY_CONST);
+       GPIO_set_pin(GPIO_B, bit_21);
+       GPIO_set_pin(GPIO_B, bit_22);
 }
 
-void red(void)
+void red(void) /**Turn on the red LED, waiting and turning it off*/
 {
-	GPIO_clear_pin(GPIO_B, bit_22);
-	delay(DELAY_CONST);
-	GPIO_set_pin(GPIO_B, bit_22);
+       GPIO_clear_pin(GPIO_B, bit_22);
+       delay(DELAY_CONST);
+       GPIO_set_pin(GPIO_B, bit_22);
 }
 
-void yellow(void)
+void yellow(void) /**Turn on the yellow LED, waiting and turning it off*/
 {
-	GPIO_clear_pin(GPIO_B, bit_22);
-	GPIO_clear_pin(GPIO_E, bit_26);
-	delay(DELAY_CONST);
-	GPIO_set_pin(GPIO_B, bit_22);
-	GPIO_set_pin(GPIO_E, bit_26);
-}
-
-
-
-void white(void)
-{
-	GPIO_clear_pin(GPIO_B, bit_21);
-	GPIO_set_pin(GPIO_B, bit_21);
-	GPIO_clear_pin(GPIO_E, bit_26);
-	GPIO_set_pin(GPIO_E, bit_26);
-	GPIO_clear_pin(GPIO_B, bit_22);
-	GPIO_set_pin(GPIO_B, bit_22);
+       GPIO_clear_pin(GPIO_B, bit_22);
+       GPIO_clear_pin(GPIO_E, bit_26);
+       delay(DELAY_CONST);
+       GPIO_set_pin(GPIO_B, bit_22);
+       GPIO_set_pin(GPIO_E, bit_26);
 }
 
 
-void white(void)
+void white(void) /**Turn on the white LED, waiting and turning it off*/
 {
-	GPIO_clear_pin(GPIO_B, bit_21);
-	GPIO_clear_pin(GPIO_B, bit_22);
-	GPIO_clear_pin(GPIO_E, bit_26);
-	delay(DELAY_CONST);
-	GPIO_set_pin(GPIO_B, bit_21);
-	GPIO_set_pin(GPIO_B, bit_22);
-	GPIO_set_pin(GPIO_E, bit_26);
+       GPIO_clear_pin(GPIO_B, bit_21);
+       GPIO_clear_pin(GPIO_B, bit_22);
+       GPIO_clear_pin(GPIO_E, bit_26);
+       delay(DELAY_CONST);
+       GPIO_set_pin(GPIO_B, bit_21);
+       GPIO_set_pin(GPIO_B, bit_22);
+       GPIO_set_pin(GPIO_E, bit_26);
 }
-
-
-
-
-
 
 
--- RGB.h
+++ RGB.h
@@ -1,19 +1,19 @@
-/*
- * RGB.h
- *
- *  Created on: 13 feb 2022
- *      Author: Mauricio Peralta O.
+/**
+	\file
+	\brief
+			This is the source file for the colors needed in the main implementation.
+	\author Mauricio Peralta Osorio
+	\date	15/02/2022
  */
 
 #ifndef RGB_H_
 #define RGB_H_
 
-void green(void); 	//Clear everything and turn on green LED
-void blue(void); 	//Clear everything and turn on blue LED
-void purple(void);	//Clear everything and turn on purple LED
-void red(void);		//Clear everything and turn on red LED
-void yellow(void);	//Clear everything and turn on yellow LED
-void white(void);	//Clear everything and turn on white LED
+void green(void); 	/**Function that turn on the green LED, waiting and turning it off*/
+void blue(void); 	/**Function that turn on the blue LED, waiting and turning it off*/
+void purple(void);	/**Function that turn on the purple LED, waiting and turning it off*/
+void red(void);		/**Function that turn on the red LED, waiting and turning it off*/
+void yellow(void);	/**Function that turn on the yellow LED, waiting and turning it off*/
+void white(void);   /**Function that turn on the white LED, waiting and turning it off*/
 
 #endif /* RGB_H_ */
-
